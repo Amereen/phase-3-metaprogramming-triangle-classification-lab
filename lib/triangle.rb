2a9e69a4529098e058eb47901ccf13c1f5f2d8af
Amereen Hanvel <amereenhanvel786@gmail.com>
2023-03-13 16:27:48 -0400

    commit tree e88a2e94ab012e11cfd51765bbcdf2c2508ebe9b

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+  attr_reader :sides
+
+  def initialize(side1, side2, side3)
+    @sides = [side1, side2, side3].sort
+    validate_triangle
+  end
+
+  def kind
+    if sides.uniq.length == 1
+      :equilateral
+    elsif sides.uniq.length == 2
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  private
+
+  def validate_triangle
+    if sides.any? { |side| side <= 0 } || sides[0] + sides[1] <= sides[2]
+      raise TriangleError.new("Invalid triangle")
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
